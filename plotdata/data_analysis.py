9d8b7fda31f21ad4e73a3886633799580634ef94
ohnickmoy <nick.moi@gmail.com>
2020-12-01 15:39:36 -0500

    Refactored data_analysis.py and started work on testing suite for data_analysis.py

--- plotdata/data_analysis.py
+++ plotdata/data_analysis.py
@@ -5,54 +5,73 @@ import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# def open_csvs(csv_dir):
-#     trip_csvs = os.listdir(csv_dir)
-#     pass
-# df = pd.read_csv('./tripdata/csv/202003-citibike-tripdata.csv')
+def get_csv_file_paths(csv_dir):
+    """takes in a file path to get files within a given directory
+    and sorts them in alpha-numerical order
 
-# pd.set_option('display.max.columns', None)
-# # print(df.head())
-# # if __name__ == "__main__":
-#     # trip_dir = "./tripdata/csv/"
-#     #open_csvs(trip_dir)
-#     # print(os.listdir(trip_dir))
-# print(len(df.index))
-# print(df['usertype'].value_counts())
-# # df['usertype'].value_counts().plot(kind='bar')
-# plt.plot(['202003', '202004', '202005'], [23, 28, 35])
-# plt.show()
+    Args:
+        csv_dir (string): string of a given file path
 
-"""get the trip_csvs
-    parse out their names, since it's uniform, split them, get the first one
-    append it to the year month combos
-    read the csv with trip_dir + file name
-    append len of index to indexCounts
-    plot and show
-"""
-TRIP_DIR = "./tripdata/csv/"
-trip_csvs = os.listdir(TRIP_DIR)
-trip_csvs.sort()
+    Returns:
+        list: sorted list of files in a directory
+    """
+    csv_paths = os.listdir(csv_dir)
+    csv_paths.sort()
 
-year_months = []
-total_trips_per_month = []
+    return csv_paths
 
-#traverse through the files in the trip_csv dir
-for file_name in trip_csvs:
-    file_list = file_name.split("-")
-    year_month = file_list[0]
-    print(f"Checking {year_month}")
-    datetime_obj = datetime.datetime.strptime(year_month[4:], "%m")
-    month_name = datetime_obj.strftime("%b")
-    year_months.append(f"{month_name} {year_month[:4]}")
+def generate_total_ridership_data(csv_paths_list, csv_dir):
+    """Generates data for total ridership of Citibike service during Covid-19 period
 
-    curr_file_path = TRIP_DIR + file_name
-    df = pd.read_csv(curr_file_path)
-    total_trips_per_month.append(len(df.index))
+    Args:
+        csv_paths_list (list): list of csv paths
+        csv_dir (string): string of directory where csvs are located
 
-graph = plt.figure(figsize=(11, 6))
-plt.plot(year_months, total_trips_per_month)
-plt.title(f"Citibike Ridership - {year_months[0]} to {year_months[len(year_months) - 1]}")
-plt.xlabel("Month and Year")
-plt.ylabel("Ridership in Millions")
+    Returns:
+        dict: dictionary of ridership data
+    """
+    year_months = []
+    total_trips_per_month = []
 
-plt.savefig('./plotdata/images/covid_ridership.png')
+    #traverse through the files in the trip_csv dir
+    for file_name in csv_paths_list:
+        file_list = file_name.split("-")
+        year_month = file_list[0]
+        print(f"Checking {year_month}")
+        datetime_obj = datetime.datetime.strptime(year_month[4:], "%m")
+        month_name = datetime_obj.strftime("%b")
+        year_months.append(f"{month_name} {year_month[:4]}")
+
+        curr_file_path = csv_dir + file_name
+        data_file = pd.read_csv(curr_file_path)
+        total_trips_per_month.append(len(data_file.index))
+
+    total_ridership_data = {
+        'year_months': year_months,
+        'total_trips_per_month': total_trips_per_month
+    }
+
+    return total_ridership_data
+
+def generate_total_ridership_chart(total_ridership_data):
+    """Generates chart for total Citibike ridership during period of Covid-19
+
+    Args:
+        total_ridership_data (dictionary): dictionary containing corresponding ridership
+        to given month and year
+    """
+    year_months = total_ridership_data['year_months']
+    total_trips_per_month = total_ridership_data['total_trips_per_month']
+    plt.figure(figsize=(11, 6))
+    plt.plot(year_months, total_trips_per_month)
+    plt.title(f"Citibike Ridership - {year_months[0]} to {year_months[len(year_months) - 1]}")
+    plt.xlabel("Month and Year")
+    plt.ylabel("Ridership in Millions")
+
+    plt.savefig('./plotdata/images/covid_ridership.png')
+
+if __name__ == '__main__':
+    CSV_PATH = './tripdata/csv/'
+    trip_csv_paths = get_csv_file_paths(CSV_PATH)
+    all_ridership_data = generate_total_ridership_data(trip_csv_paths, CSV_PATH)
+    generate_total_ridership_chart(all_ridership_data)
---
+++ test/test_data_analysis.py
@@ -0,0 +1,15 @@
+"""Test suite for data_analysis.py
+"""
+
+import unittest
+import os
+from plotdata import data_analysis
+
+class TestDataAnalysis(unittest.TestCase):
+    """Class testing suite for data_analysis.py
+    """
+    def test_get_csv_file_paths(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
